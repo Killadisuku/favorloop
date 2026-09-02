53d5a29ac818d8288c1e11a2de179de73d3bb7f4
Killadisuku <139642675+Killadisuku@users.noreply.github.com>
2026-09-02 12:38:53 +0400

    Fix start screen layout with dedicated styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import './start.css'
 import { bootLive } from './liveBoot'
 import { StoreProvider } from './store'
 
